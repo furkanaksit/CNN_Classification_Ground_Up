225610f1cf2c16c0ac886228f14e11f02077cc3d
Ahmet Çağrı Kılıç <cagriiikilic@gmail.com>
2019-01-03 01:07:14 +0300

    Update CNN_Classificator.py

--- CNN_Classificator.py
+++ CNN_Classificator.py
@@ -139,5 +139,136 @@ test_labels = test_labels[indices_test]
 encoded=one_hot_encode(train_labels)
 encoded_test=one_hot_encode(test_labels)
 
+#%%    making and training the model
+
+# we took training data as batches 100 images 
+batch_size = 100
+num_classes = len(image_dataset[1])
+# we chose 100 epochs because we dont have many images
+epochs = 100
+data_augmentation = True
+num_predictions = 20
+
+
+model = Sequential()
+
+# convolutional layer applies different filters 
+model.add(Conv2D(32, (3, 3), padding='same', input_shape=(200,200,3)))
+
+# reLu makes the negative values 0 and positive values stays the same
+model.add(Activation('relu'))
+
+# pooling makes the image smaller as it tooks the max value in 2 by 2 matrix
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(128, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+# flatten takes all the outputs from all layers
+# and converts it to [1,n] array
+# dense layer is the neural network itself
+# it does assign the weights and biases and with backpropagation it updates
+# all the weigths, biases and filters
+model.add(Flatten())
+model.add(Dense(128))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(256))
+model.add(Activation('relu'))
+model.add(Dense(512))
+model.add(Dropout(0.5))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
+model.summary()
+
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+
+
+
+
+datagen =   ImageDataGenerator(
+                rotation_range=10.,
+                width_shift_range=0.1,
+                height_shift_range=0.1,
+                shear_range=0.,
+                zoom_range=1.,
+                horizontal_flip=True,
+                vertical_flip=True)
+
+# we store loss and acc data to be able to plot the data 
+history = model.fit_generator(datagen.flow(train, encoded, batch_size = batch_size),
+                              steps_per_epoch  = int(np.ceil(train.shape[0] / float(batch_size))),
+                              epochs = epochs,
+                              validation_data = (test, encoded_test),
+                              workers = 4)
+ 
+model.evaluate(test, encoded_test)
+
+# we can plot the history of losses and accuracy as we stored it
+# Loss Curves
+plt.figure(figsize=[8,6])
+plt.plot(history.history['loss'],'r',linewidth=3.0)
+plt.plot(history.history['val_loss'],'b',linewidth=3.0)
+plt.legend(['Training loss', 'Validation Loss'],fontsize=18)
+plt.xlabel('Epochs ',fontsize=16)
+plt.ylabel('Loss',fontsize=16)
+plt.title('Loss Curves',fontsize=16)
+ 
+# Accuracy Curves
+plt.figure(figsize=[8,6])
+plt.plot(history.history['acc'],'r',linewidth=3.0)
+plt.plot(history.history['val_acc'],'b',linewidth=3.0)
+plt.legend(['Training Accuracy', 'Validation Accuracy'],fontsize=18)
+plt.xlabel('Epochs ',fontsize=16)
+plt.ylabel('Accuracy',fontsize=16)
+plt.title('Accuracy Curves',fontsize=16)
+
+#%%   for saving the model
+
+# we show the loss and acc before we save it 
+scores = model.evaluate(test, encoded_test, verbose=0)
+print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+# we save the model in json file and weights in hdf5 file
+
+# serialize model to JSON
+model_json = model.to_json()
+with open("./model.json", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+model.save_weights("./model.h5")
+print("Saved model to disk")
+
+#%%   for loading the model
+# we can use this module to load the model and weights
+# because we cant train it every single time 
+# so once you train it you can still use the model to make predictions 
+
+# load json and create model
+json_file = open('./model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights("./model.h5")
+print("Loaded model from disk")
+
+# evaluate loaded model on test data
+loaded_model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+score = loaded_model.evaluate(test, encoded_test, verbose=0)
+print("%s: %.2f%%" % (loaded_model.metrics_names[0], score[0]*100))
+print("%s: %.2f%%" % (loaded_model.metrics_names[1], score[1]*100))
+
+
     
 
